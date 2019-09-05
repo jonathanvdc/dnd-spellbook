765d19f9e88c8573d8e24f4d9de341b81877e342
jonathanvdc <jonathan.vdc@outlook.com>
2019-09-05 19:14:35 +0200

    Update spell cards

--- src/SpellCard.tsx
+++ src/SpellCard.tsx
@@ -6,36 +6,74 @@ type Props = {
     spell: Spell;
 };
 
+/**
+ * A card that describes a spell.
+ */
 class SpellCard extends Component<Props, {}> {
+    /**
+     * Create a "quick stats" grid to include in the card.
+     */
+    createPropertiesGrid() {
+        let components = [];
+        if (this.props.spell.components.verbal) {
+            components.push("V");
+        }
+        if (this.props.spell.components.somatic) {
+            components.push("S");
+        }
+        if (this.props.spell.components.material) {
+            components.push("M");
+        }
+        return <div className="SpellPropertiesGrid">
+            <div className="SpellPropertyItem">
+                <div className="SpellPropertyItemName">Casting time</div>
+                <div>{this.props.spell.casting_time}</div>
+            </div>
+            <div className="SpellPropertyItem">
+                <div className="SpellPropertyItemName">Range</div>
+                <div>{this.props.spell.range}</div>
+            </div>
+            <div className="SpellPropertyItem">
+                <div className="SpellPropertyItemName">Components</div>
+                <div>{components.join(", ")}</div>
+            </div>
+            <div className="SpellPropertyItem">
+                <div className="SpellPropertyItemName">Duration</div>
+                <div>{this.props.spell.duration}</div>
+            </div>
+        </div>;
+    }
+
+    createFactFootnote(key: string, value: string) {
+        return <div className="SpellFootnote"><span className="SpellPropertyItemName">{key}:</span> {value}</div>;
+    }
+
+    /**
+     * Creates an "additional facts" sheet, which is essentially a collection
+     * of footnotes.
+     */
+    createFootnotes(): JSX.Element[] {
+        let elems: JSX.Element[] = [];
+        if (this.props.spell.components.materials_needed) {
+            elems.push(this.createFactFootnote("Materials", this.props.spell.components.materials_needed.join("; ")));
+        }
+        elems.push(this.createFactFootnote("Classes", this.props.spell.classes.join(", ")));
+        return elems;
+    }
+
     render() {
         let imageUrlPrefix = "https://raw.githubusercontent.com/iconsheets/iconsheets.github.io/master/Images/";
         let imageUrl = imageUrlPrefix + this.props.spell.name + ".png";
-        console.log(imageUrl);
         return <div className="SpellCardPanel">
             <img className="SpellThumbnail" src={imageUrl} alt={this.props.spell.name + " icon"} />
-            <div className="SpellType">{this.props.spell.type}</div>
+            <div className="SpellType">{this.props.spell.type + (this.props.spell.ritual ? " (ritual)" : "")}</div>
             <div className="SpellName">{this.props.spell.name}</div>
             <hr/>
-            <div className="SpellPropertiesGrid">
-                <div className="SpellPropertyItem">
-                    <div className="SpellPropertyItemName">Casting time</div>
-                    <div>{this.props.spell.casting_time}</div>
-                </div>
-                <div className="SpellPropertyItem">
-                    <div className="SpellPropertyItemName">Range</div>
-                    <div>{this.props.spell.range}</div>
-                </div>
-                <div className="SpellPropertyItem">
-                    <div className="SpellPropertyItemName">Components</div>
-                    <div>{this.props.spell.components.raw}</div>
-                </div>
-                <div className="SpellPropertyItem">
-                    <div className="SpellPropertyItemName">Duration</div>
-                    <div>{this.props.spell.duration}</div>
-                </div>
-            </div>
+            {this.createPropertiesGrid()}
             <hr/>
             <p className="SpellDescription">{this.props.spell.description}</p>
+            <hr/>
+            {this.createFootnotes()}
         </div>;
     }
 }
