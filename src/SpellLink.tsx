07881c8d1a6b006100f611fb17c69c3e4dfe2af0
jonathanvdc <jonathan.vdc@outlook.com>
2019-09-06 11:23:18 +0200

    Add a hover effect to spell links

--- src/SpellLink.tsx
+++ src/SpellLink.tsx
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
 import { Spell, getSpellThumbnailUrl } from "./spell";
 import { Link } from "react-router-dom";
+import ReactHover from "react-hover";
 import './SpellLink.css';
+import SpellCard from "./SpellCard";
 
 /**
  * A reference to a spell, formatted as an image.
  */
 class SpellLink extends Component<{spell: Spell}, {}> {
     render() {
-        return <Link className="SpellLink" to={`/spell/${this.props.spell.name}`}>
-            <img src={getSpellThumbnailUrl(this.props.spell)} alt={this.props.spell.name + " thumbnail"} />
-            <div>{this.props.spell.name}</div>
-        </Link>;
+        const hoverOptions = {
+            followCursor: false,
+            shiftX: 20,
+            shiftY: 0
+        };
+        let spellLink = `/spell/${this.props.spell.name}`;
+        return <ReactHover
+            options={hoverOptions}>
+            <ReactHover.Trigger type='trigger'>
+                <Link className="SpellLink" to={spellLink}>
+                    <img src={getSpellThumbnailUrl(this.props.spell)} alt={this.props.spell.name + " thumbnail"} />
+                    <div>{this.props.spell.name}</div>
+                </Link>
+            </ReactHover.Trigger>
+            <ReactHover.Hover type='hover'>
+                <Link className="ExtraSpellLink" to={spellLink}>
+                    <SpellCard spell={this.props.spell} />
+                </Link>
+            </ReactHover.Hover>
+            </ReactHover>;
     }
 }
 
