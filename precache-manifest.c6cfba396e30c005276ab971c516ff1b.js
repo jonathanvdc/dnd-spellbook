55cbf1ba73dbfbfba9c5c304cdb69598326d3762
jonathanvdc <jonathanvdc@users.noreply.github.com>
2019-09-06 08:48:24 +0000

    Updates

---
+++ precache-manifest.c6cfba396e30c005276ab971c516ff1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2c29d4f43f605188e905ecd7542885",
+    "url": "/dnd-spellbook/index.html"
+  },
+  {
+    "revision": "727f3f49005ffb1a5e73",
+    "url": "/dnd-spellbook/static/css/main.5847c881.chunk.css"
+  },
+  {
+    "revision": "15effebb567309e58980",
+    "url": "/dnd-spellbook/static/js/2.a277ba37.chunk.js"
+  },
+  {
+    "revision": "727f3f49005ffb1a5e73",
+    "url": "/dnd-spellbook/static/js/main.5e2411ff.chunk.js"
+  },
+  {
+    "revision": "e30fbd852c4a018865b1",
+    "url": "/dnd-spellbook/static/js/runtime~main.318ddfa0.js"
+  }
+]);
