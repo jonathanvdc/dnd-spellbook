aac50b9eb0a420b9d73a7eaa5424a40939de476f
jonathanvdc <jonathanvdc@users.noreply.github.com>
2019-09-16 12:30:53 +0000

    Updates

---
+++ precache-manifest.83a16382abe0264475f59616945f9788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccea979e81a4a7b29e63eaa03158f983",
+    "url": "/dnd-spellbook/index.html"
+  },
+  {
+    "revision": "93614577f91fdec9814c",
+    "url": "/dnd-spellbook/static/css/3.e17c4c79.chunk.css"
+  },
+  {
+    "revision": "0384c75c66ab830fe0ef",
+    "url": "/dnd-spellbook/static/css/main.cb1cc120.chunk.css"
+  },
+  {
+    "revision": "adee0cc7370920980ddd",
+    "url": "/dnd-spellbook/static/js/2.05e131b0.chunk.js"
+  },
+  {
+    "revision": "93614577f91fdec9814c",
+    "url": "/dnd-spellbook/static/js/3.206d88e0.chunk.js"
+  },
+  {
+    "revision": "0384c75c66ab830fe0ef",
+    "url": "/dnd-spellbook/static/js/main.fd06da33.chunk.js"
+  },
+  {
+    "revision": "596cbf014a39b4412ee6",
+    "url": "/dnd-spellbook/static/js/runtime~main.aee598f8.js"
+  }
+]);
